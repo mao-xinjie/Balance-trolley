a709e3c7881accbb0196eceb2b6645e51cdbd946
MaoXinjie <1291430698@qq.com>
2023-02-06 15:24:45 +0800

    update software

---
+++ Software/平衡小车V1/Hardware/MPU6050.c
@@ -0,0 +1,83 @@
+#include "stm32f10x.h"                  // Device header
+#include "MyI2C.h"
+#include "MPU6050_Reg.h"
+
+#define MPU6050_ADDRESS		0xD0
+
+void MPU6050_WriteReg(uint8_t RegAddress, uint8_t Data)
+{
+	MyI2C_Start();
+	MyI2C_SendByte(MPU6050_ADDRESS);
+	MyI2C_ReceiveAck();
+	MyI2C_SendByte(RegAddress);
+	MyI2C_ReceiveAck();
+	MyI2C_SendByte(Data);
+	MyI2C_ReceiveAck();
+	MyI2C_Stop();
+}
+
+uint8_t MPU6050_ReadReg(uint8_t RegAddress)
+{
+	uint8_t Data;
+	
+	MyI2C_Start();
+	MyI2C_SendByte(MPU6050_ADDRESS);
+	MyI2C_ReceiveAck();
+	MyI2C_SendByte(RegAddress);
+	MyI2C_ReceiveAck();
+	
+	MyI2C_Start();
+	MyI2C_SendByte(MPU6050_ADDRESS | 0x01);
+	MyI2C_ReceiveAck();
+	Data = MyI2C_ReceiveByte();
+	MyI2C_SendAck(1);
+	MyI2C_Stop();
+	
+	return Data;
+}
+
+void MPU6050_Init(void)
+{
+	MyI2C_Init();
+	MPU6050_WriteReg(MPU6050_PWR_MGMT_1, 0x01);
+	MPU6050_WriteReg(MPU6050_PWR_MGMT_2, 0x00);
+	MPU6050_WriteReg(MPU6050_SMPLRT_DIV, 0x09);
+	MPU6050_WriteReg(MPU6050_CONFIG, 0x06);
+	MPU6050_WriteReg(MPU6050_GYRO_CONFIG, 0x18);
+	MPU6050_WriteReg(MPU6050_ACCEL_CONFIG, 0x18);
+}
+
+uint8_t MPU6050_GetID(void)
+{
+	return MPU6050_ReadReg(MPU6050_WHO_AM_I);
+}
+
+void MPU6050_GetData(int16_t *AccX, int16_t *AccY, int16_t *AccZ, 
+						int16_t *GyroX, int16_t *GyroY, int16_t *GyroZ)
+{
+	uint8_t DataH, DataL;
+	
+	DataH = MPU6050_ReadReg(MPU6050_ACCEL_XOUT_H);
+	DataL = MPU6050_ReadReg(MPU6050_ACCEL_XOUT_L);
+	*AccX = (DataH << 8) | DataL;
+	
+	DataH = MPU6050_ReadReg(MPU6050_ACCEL_YOUT_H);
+	DataL = MPU6050_ReadReg(MPU6050_ACCEL_YOUT_L);
+	*AccY = (DataH << 8) | DataL;
+	
+	DataH = MPU6050_ReadReg(MPU6050_ACCEL_ZOUT_H);
+	DataL = MPU6050_ReadReg(MPU6050_ACCEL_ZOUT_L);
+	*AccZ = (DataH << 8) | DataL;
+	
+	DataH = MPU6050_ReadReg(MPU6050_GYRO_XOUT_H);
+	DataL = MPU6050_ReadReg(MPU6050_GYRO_XOUT_L);
+	*GyroX = (DataH << 8) | DataL;
+	
+	DataH = MPU6050_ReadReg(MPU6050_GYRO_YOUT_H);
+	DataL = MPU6050_ReadReg(MPU6050_GYRO_YOUT_L);
+	*GyroY = (DataH << 8) | DataL;
+	
+	DataH = MPU6050_ReadReg(MPU6050_GYRO_ZOUT_H);
+	DataL = MPU6050_ReadReg(MPU6050_GYRO_ZOUT_L);
+	*GyroZ = (DataH << 8) | DataL;
+}
--- Software/平衡小车V1/User/main.c
+++ Software/平衡小车V1/User/main.c
@@ -1,33 +1,80 @@
+//#include "stm32f10x.h"                  // Device header
+//#include "Delay.h"
+//#include "OLED.h"
+//#include "AD.h"
+//#include "Serial.h"
+
+//uint16_t ADValue;
+//float Voltage;
+
+
+//int sensor[6]={0,0,0,0,0,0};
+
+//int main(void)
+//{
+//	OLED_Init();
+//	AD_Init();
+//	Serial_Init();
+//	Serial_SendByte(0x41);
+//	printf("\r\n%d", 222);
+//	
+//	OLED_ShowString(1, 1, "ADValue:");
+//	OLED_ShowString(2, 1, "Volatge:00.00V");
+//	
+//	while (1)
+//	{
+//		ADValue = AD_GetValue();
+//		Voltage = (float)ADValue / 4095 * 12;
+//		
+//		printf("\r\n%d", -ADValue);
+//		OLED_ShowNum(1, 9, ADValue, 4);
+//		OLED_ShowNum(2, 9, Voltage, 2);
+//		OLED_ShowNum(2, 12, (uint16_t)(Voltage * 100) % 100, 2);
+//		
+//		Delay_ms(100);
+//	}
+//}
+
+
+
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
 #include "OLED.h"
-#include "AD.h"
+#include "MPU6050.h"
 #include "Serial.h"
 
-uint16_t ADValue;
-float Voltage;
-
+uint8_t ID;
+int16_t AX, AY, AZ, GX, GY, GZ;
 int sensor[6]={0,0,0,0,0,0};
-
 int main(void)
 {
 	OLED_Init();
-	AD_Init();
+	MPU6050_Init();
 	Serial_Init();
-	Serial_SendByte(0x41);
 	
-	OLED_ShowString(1, 1, "ADValue:");
-	OLED_ShowString(2, 1, "Volatge:00.00V");
+	OLED_ShowString(1, 1, "ID:");
+	ID = MPU6050_GetID();
+	OLED_ShowHexNum(1, 4, ID, 2);
 	
 	while (1)
 	{
-		ADValue = AD_GetValue();
-		Voltage = (float)ADValue / 4095 * 12;
+		MPU6050_GetData(&AX, &AY, &AZ, &GX, &GY, &GZ);
 		
-		OLED_ShowNum(1, 9, ADValue, 4);
-		OLED_ShowNum(2, 9, Voltage, 2);
-		OLED_ShowNum(2, 12, (uint16_t)(Voltage * 100) % 100, 2);
+		OLED_ShowSignedNum(2, 1, AX, 5);
 		
-		Delay_ms(100);
+		OLED_ShowSignedNum(3, 1, AY, 5);
+		
+		OLED_ShowSignedNum(4, 1, AZ, 5);
+		OLED_ShowSignedNum(2, 8, GX, 5);
+		OLED_ShowSignedNum(3, 8, GY, 5);
+		OLED_ShowSignedNum(4, 8, GZ, 5);
+		
+		printf("date:%d,", AX);
+		printf("%d,", AY);
+		printf("%d,", AZ);
+		printf("%d,", GX);
+		printf("%d,", GY);
+		printf("%d\r\n", GZ);
 	}
 }
+
